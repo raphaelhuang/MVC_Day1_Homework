6751970e1728f65de28a477d63799cf0fe38ed61
Raphael <huang145@hotmail.com>
2016-04-25 00:58:13 +0800

    Modify Index layout and add Validation

---
+++ MVC_Day1_Homework/Controllers/ValidController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Day1_Homework.Controllers
+{
+    public class ValidController : Controller
+    {
+        // GET: Valid
+        public ActionResult Index(DateTime date)
+        {
+            bool isValidate = (DateTime.Compare(date, DateTime.Now)) < 0;
+
+            return Json(isValidate, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
--- MVC_Day1_Homework/ViewModels/AssetViewModel.cs
+++ MVC_Day1_Homework/ViewModels/AssetViewModel.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MVC_Day1_Homework.ViewModels
 {
     public class AssetViewModel
     {
+        [Required]
         public int Category { get; set; }
+       
+        [Required]
+        [RegularExpression(@"\d+$", ErrorMessage = "請輸入正整數")]
+        [Range(0, int.MaxValue, ErrorMessage = "請輸入正整數")]
         public decimal Money { get; set; }
+
+        [Required]
+        [Remote("Index", "Valid", ErrorMessage = "日期不得大於今天")]
         public DateTime Date { get; set; }
+        
+        [Required]
+        [MaxLength(100, ErrorMessage = "最多輸入100個字元")]
         public string Description { get; set; }
 
     }
