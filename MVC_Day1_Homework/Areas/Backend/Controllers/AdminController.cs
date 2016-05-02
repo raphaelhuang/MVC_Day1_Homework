3a930b7a9dcad4881e6a67365f4b52cec2172078
Raphael <huang145@hotmail.com>
2016-05-02 13:50:13 +0800

    Add Area "Backend" and AdminController

---
+++ MVC_Day1_Homework/Areas/Backend/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Day1_Homework.Areas.Backend.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Backend/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
